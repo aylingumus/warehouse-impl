6f1bff8e7aabb26122ea63b69451bbfc39637023
aylingumus <aylingumus7@gmail.com>
2022-10-19 01:35:08 +0200

    Change resource names

--- src/main/kotlin/warehouseapp/controller/ProductController.kt
+++ src/main/kotlin/warehouseapp/controller/ProductController.kt
@@ -2,14 +2,16 @@ package warehouseapp.controller
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import warehouseapp.model.Product
 import warehouseapp.service.ProductService
 
 @RestController
+@RequestMapping("warehouse")
 class ProductController(private val productService: ProductService) {
 
     @GetMapping("/available-products")
@@ -17,10 +19,10 @@ class ProductController(private val productService: ProductService) {
         val availableProducts = productService.getAvailableProducts()
         return if (availableProducts.size > 0) {
             ResponseEntity.ok(availableProducts)
-        } else ResponseEntity("No available products", HttpStatus.NO_CONTENT)
+        } else ResponseEntity("No available products.", HttpStatus.NO_CONTENT)
     }
 
-    @DeleteMapping("/")
+    @PutMapping("/sell-product")
     fun sellProduct(@RequestParam id: Int): ResponseEntity<*> {
         val product: Product? = productService.sellProduct(id)
         return if (product == null) {
